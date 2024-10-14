ae7f1b5608a47a80d767f1680cccedea8286064f
AbdullahElmasry <abdallhking3040@gmail.com>
2024-10-14 22:06:17 +0300

    find a string.py solved

--- Python/Find a string.py
+++ Python/Find a string.py
@@ -2,18 +2,17 @@ def count_substring(string, sub_string):
     # Input:
     # DCDC
     # CDC
-    
+
     counter = 0
     copy_string = string
-    ls_copy_string = list(copy_string) # turning to list because string is immutable from assigning
-
 
     for _ in range(len(string)):
         index = copy_string.find(sub_string) # if not found returns -1
         if index != -1:
             counter += 1
-            del ls_copy_string[0:index+1] # delete the elements before the first occurance including the first letter of the subString
-    
+            copy_string = list(copy_string) # turning to list because string is immutable from assigning
+            del copy_string[0:index+1] # delete the elements before the first occurance including the first letter of the subString
+            copy_string = "".join(copy_string)
     
     return counter
 
