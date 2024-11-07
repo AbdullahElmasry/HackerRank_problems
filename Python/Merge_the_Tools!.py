8935bf54c365d729e5e50b9b510499c9c7e681f0
abdullahelmasry <abdallhking3040@gmail.com>
2024-11-07 13:28:38 +0200

    Merge_the_Tools! >> under construction

--- Python/Merge_the_Tools!.py
+++ Python/Merge_the_Tools!.py
@@ -9,15 +9,29 @@ AD
 
 separated = []
 temp = []
+result = []
 
 def merge_the_tools(string, k):
     for i in range(len(string)):
         temp.append(string[i])
         if (i+1) % k == 0:
-            separated.append(temp)
+            separated.append(temp[:]) # we save copy of tmep bc of dear python is not saving a copy of the values, it instead save them by refrence, that's mean that if we edit the source the others will be affected
             temp.clear()
     print(separated)
+# separated = [['A', 'A', 'B'], ['C', 'A', 'A'], ['A', 'D', 'A']]
+    for part in separated:
+        cp_part = part.sort()
+        for i in range(len(part)):
+            if cp_part[i] != cp_part[i-1] or i == 0: 
+                result.append(part[i])
     
+    
+    print(result)
+            
+
+    # print(f"this is the result: {result}")
+
+        
 
 s = "AABCAAADA"
 k = 3
